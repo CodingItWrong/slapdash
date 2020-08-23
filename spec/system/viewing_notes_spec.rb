30a5e81d8a4c109b14bdde9a7a31ed8cd45bcfcc
Josh Justice <me@codingitwrong.com>
2020-08-23 19:39:08 -0400

    Confirm list is only one user's notes

--- spec/system/viewing_notes_spec.rb
+++ spec/system/viewing_notes_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Viewing Notes', type: :system do
     driven_by(:rack_test)
   end
 
-  it 'allows viewing a note' do
+  it 'allows navigating directly to a note' do
     user_name = 'someuser'
     note_title = 'Note Title'
     note_slug = 'note-title'
@@ -19,14 +19,30 @@ RSpec.describe 'Viewing Notes', type: :system do
       body: note_body,
     )
 
-    visit "/#{user_name}"
-
-    click_on note_title
+    visit "/#{user_name}/#{note_slug}"
 
     expect(page).to have_current_path(
       "/#{user.display_name}/#{note_slug}"
     )
     expect(page).to have_content(note_title)
     expect(page).to have_content(note_body)
+  end
+
+  it "allows listing a user's notes" do
+    user = FactoryBot.create(:user)
+    user_note = FactoryBot.create(:note, user: user)
+    other_user_note = FactoryBot.create(:note)
+
+    visit "/#{user.display_name}"
+
+    expect(page).not_to have_content(other_user_note.title)
+
+    click_on user_note.title
+
+    expect(page).to have_current_path(
+      "/#{user.display_name}/#{user_note.slug}"
+    )
+    expect(page).to have_content(user_note.title)
+    expect(page).to have_content(user_note.body)
   end
 end
