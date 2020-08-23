a021eb15ffb7d7dca160d800c9e3e1fff971bd97
Josh Justice <me@codingitwrong.com>
2020-08-23 19:42:46 -0400

    Seed default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'example@example.com',
+  password: 'password',
+  display_name: 'example',
+)
