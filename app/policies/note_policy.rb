8eb50d3d475cc72957aa29be7af7079204ac2208
Josh Justice <me@codingitwrong.com>
2020-08-27 12:35:58 -0400

    Use alias for method delegation

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
 class NotePolicy < ApplicationPolicy
-  def create?
-    own_record?
-  end
-
-  def update?
-    own_record?
-  end
-
-  def destroy?
-    own_record?
-  end
-
   def own_record?
     record.user == user
   end
+
+  alias :create? :own_record?
+  alias :update? :own_record?
+  alias :destroy? :own_record?
 end
