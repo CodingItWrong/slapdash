d319b7af0daf80c3b19c11da6b944d01cbd154a1
Josh Justice <me@codingitwrong.com>
2020-08-31 07:06:44 -0400

    Remove duplication in uniqueness tests

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,13 +2,14 @@
 
 class Note < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+
+  belongs_to :user
+
+  friendly_id :title, use: :scoped, scope: :user
 
   def should_generate_new_friendly_id?
     title_changed?
   end
-
-  belongs_to :user
 
   validates :title, presence: true
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -34,16 +34,27 @@ RSpec.describe Note, type: :model do
       expect(note.slug).to eq(old_slug)
     end
 
-    it 'cannot be duplicated by the same users' do
-      user = FactoryBot.create(:user)
+    context 'uniqueness' do
+      let(:user1) { FactoryBot.create(:user) }
+      let(:user2) { FactoryBot.create(:user) }
+      let(:title) { 'My Note' }
+      let(:slug) { 'my-note' }
 
-      title = 'My Note'
-      slug = 'my-note'
-      note1 = FactoryBot.create(:note, user: user, title: title)
-      note2 = FactoryBot.create(:note, user: user, title: title)
+      it 'cannot be duplicated by the same users' do
+        note1 = FactoryBot.create(:note, user: user1, title: title)
+        note2 = FactoryBot.create(:note, user: user1, title: title)
 
-      expect(note1.slug).to eq(slug)
-      expect(note2.slug).not_to eq(slug)
+        expect(note1.slug).to eq(slug)
+        expect(note2.slug).not_to eq(slug)
+      end
+
+      it 'can be duplicated across users' do
+        user1note = FactoryBot.create(:note, user: user1, title: title)
+        user2note = FactoryBot.create(:note, user: user2, title: title)
+
+        expect(user1note.slug).to eq(slug)
+        expect(user2note.slug).to eq(slug)
+      end
     end
   end
 end
