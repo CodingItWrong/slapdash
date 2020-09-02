8724b48c2b02e7ad904df64b3a8d23c127f9adb7
Josh Justice <me@codingitwrong.com>
2020-09-02 08:13:38 -0400

    Redirect from root route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       redirect_to notes_path(current_user.display_name)
+    else
+      redirect_to new_user_session_path
     end
   end
 end
