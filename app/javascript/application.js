55cfe9454566d053bacf16f54ef0584045025324
Josh Justice <me@codingitwrong.com>
2024-03-26 16:41:35 -0400

    Do linting and formatting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "@hotwired/turbo-rails"
-import "prism/prism"
-import * as bootstrap from "bootstrap"
+import '@hotwired/turbo-rails';
+import 'prism/prism';
 
-Prism.manual = true
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap';
 
-document.addEventListener("turbo:load", function(){
+Prism.manual = true;
+
+document.addEventListener('turbo:load', function () {
   Prism.highlightAll();
 });
 
-document.addEventListener("turbo:frame-render", function(){
+document.addEventListener('turbo:frame-render', function () {
   Prism.highlightAll();
 });
-import "controllers"
+import 'controllers';
--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from '@hotwired/stimulus';
 
 export default class FilterController extends Controller {
-  static targets = ["filterTextField", "listItem"]
+  static targets = ['filterTextField', 'listItem'];
 
   connect() {
-    this.filterTextFieldTarget.focus()
+    this.filterTextFieldTarget.focus();
   }
 
   update() {
-    const filterText = this.filterTextFieldTarget.value.trim().toLowerCase()
+    const filterText = this.filterTextFieldTarget.value.trim().toLowerCase();
     for (let listItem of this.listItemTargets) {
-      const itemText = listItem.innerText.trim().toLowerCase()
-      const shouldHide = filterText && !itemText.includes(filterText)
-      listItem.classList.toggle("d-none", shouldHide)
+      const itemText = listItem.innerText.trim().toLowerCase();
+      const shouldHide = filterText && !itemText.includes(filterText);
+      listItem.classList.toggle('d-none', shouldHide);
     }
   }
 }
