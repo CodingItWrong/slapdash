97ce811332b8eb5ffe9d2f573a10788369206266
Josh Justice <me@codingitwrong.com>
2024-03-26 16:35:12 -0400

    Move prism to vendor folder

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin_all_from "app/javascript/prism", under: "prism"
+pin_all_from "vendor/javascript/prism", under: "prism"
 pin "bootstrap", to: "bootstrap.min.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
