de011cd1479f77f1246e888577b9f558fada3e8a
Josh Justice <me@codingitwrong.com>
2024-03-26 13:08:53 -0400

    Apply Prism after turbo load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+import "@hotwired/turbo-rails"
 import "prism/prism"
 import * as bootstrap from "bootstrap"
 
 Prism.manual = true
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("turbo:load", function(){
   Prism.highlightAll();
 });
-import "@hotwired/turbo-rails"
